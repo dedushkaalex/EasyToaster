d17bdf1c926983126d74bfab08a91d0dbd99f9b0
A1exxuss <32575876+dedushkaalex@users.noreply.github.com>
2024-06-25 21:08:58 +0300

    Revert "feat: config"

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import observable from 'core/observable.ts';
+/* eslint-disable react/button-has-type */
 
-import { useState } from 'react';
+/* eslint-disable react/jsx-no-target-blank */
+import React, { useState } from "react";
+
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import { Notification } from "./components/Notification";
+import "./style.css";
+import viteLogo from "/vite.svg";
+
+import observable from "./core/observable";
 
 function handleClick() {
-  observable.notify('User clicked button!');
+  observable.notify("User clicked button!");
 }
 
 function logger(data) {
@@ -15,48 +24,40 @@ observable.subscribe(logger);
 function App() {
   const [count, setCount] = useState(0);
   const [notifications, setNotifications] = useState([
-    { id: 1, message: 'This is a success message!', type: 'success' },
-    { id: 2, message: 'This is an error message!', type: 'error' },
-    { id: 3, message: 'This is an info message!', type: 'info' },
+    { id: 1, message: "This is a success message!", type: "success" },
+    { id: 2, message: "This is an error message!", type: "error" },
+    { id: 3, message: "This is an info message!", type: "info" }
   ]);
 
-  const removeNotification = id => {
-    setNotifications(
-      notifications.filter(notification => notification.id !== id),
-    );
+  const removeNotification = (id) => {
+    setNotifications(notifications.filter((notification) => notification.id !== id));
   };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+        <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
+        <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button
-          onClick={() => {
-            handleClick();
-            setCount(count => count + 1);
-          }}
-        >
-          count is {count}
-        </button>
+        <button onClick={() => {
+          handleClick();
+          setCount((count) => count + 1);
+        }}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
 
       {/* TODO: создать врапер для постоянного контейнера */}
       <div className="notification-container">
-        {notifications.map(notification => (
+        {notifications.map((notification) => (
           <Notification
             key={notification.id}
             message={notification.message}
