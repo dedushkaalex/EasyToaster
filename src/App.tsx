554080308e930bb90f10a7a81531561d5afb761a
Aleksei Erchenko <proger@a1exxuss.ru>
2024-06-25 16:51:01 +0300

    Добавил обсервер

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
 /* eslint-disable react/button-has-type */
 
 /* eslint-disable react/jsx-no-target-blank */
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './App.css';
-import reactLogo from './assets/react.svg';
-import { Notification } from './components/Notification';
-import './style.css';
-import viteLogo from '/vite.svg';
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import { Notification } from "./components/Notification";
+import "./style.css";
+import viteLogo from "/vite.svg";
+
+import observable from "./core/observable";
+
+function handleClick() {
+  observable.notify("User clicked button!");
+}
+
+function logger(data) {
+  console.log(`${Date.now()} ${data}`);
+}
+
+observable.subscribe(logger);
 
 function App() {
   const [count, setCount] = useState(0);
   const [notifications, setNotifications] = useState([
-    { id: 1, message: 'This is a success message!', type: 'success' },
-    { id: 2, message: 'This is an error message!', type: 'error' },
-    { id: 3, message: 'This is an info message!', type: 'info' },
+    { id: 1, message: "This is a success message!", type: "success" },
+    { id: 2, message: "This is an error message!", type: "error" },
+    { id: 3, message: "This is an info message!", type: "info" }
   ]);
 
   const removeNotification = (id) => {
@@ -33,7 +45,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button onClick={() => {
+          handleClick();
+          setCount((count) => count + 1);
+        }}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
